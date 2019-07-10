16a9e1347befe212274a3f130fd3d61eadbbebdd
xiaolei <865079106@qq.com>
2019-07-10 15:55:46 +0800

    集成movie组件

---
+++ app_movie/src/main/java/com/xiaolei/unit/app_movie/MovieMainActivity.java
@@ -0,0 +1,14 @@
+package com.xiaolei.unit.app_movie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaolei.unit.baselibrary.base.BaseActivity;
+
+public class MovieMainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_movie_main;
+    }
+}
