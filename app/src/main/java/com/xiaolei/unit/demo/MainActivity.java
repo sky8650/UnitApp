5539c0c0e6d38e793db215c951e59b3c8f3bb37c
xiaolei <865079106@qq.com>
2019-07-25 14:28:32 +0800

    集成movie组件

--- app/src/main/java/com/xiaolei/unit/demo/MainActivity.java
+++ app/src/main/java/com/xiaolei/unit/demo/MainActivity.java
@@ -3,7 +3,6 @@ package com.xiaolei.unit.demo;
 import com.xiaolei.unit.baselibrary.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
-
     @Override
     protected int getLayout() {
         return R.layout.activity_main;
--- app_movie/src/main/java/com/xiaolei/unit/app_movie/ui/MovieMainActivity.java
+++ app_movie/src/main/java/com/xiaolei/unit/app_movie/ui/MovieMainActivity.java
@@ -1,12 +1,44 @@
 package com.xiaolei.unit.app_movie.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.xiaolei.unit.app_movie.R;
+import com.xiaolei.unit.app_movie.R2;
 import com.xiaolei.unit.baselibrary.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MovieMainActivity extends BaseActivity {
+    @BindView(R2.id.lib_tv)
+    TextView  lib_tv;
+
 
     @Override
     protected int getLayout() {
         return R.layout.activity_movie_main;
     }
+
+    @Override
+    protected void initEventAndData(Bundle savedInstanceState) {
+        super.initEventAndData(savedInstanceState);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R2.id.lib_tv})
+    public void onViewClicked(View view) {
+        if (view.getId() == R.id.lib_tv) {
+            Toast.makeText(MovieMainActivity.this, "点击TextView", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
 }
